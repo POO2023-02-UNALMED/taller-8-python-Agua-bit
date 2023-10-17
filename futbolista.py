4fbad9fd1c36be8a4a98c0a63e9f6c2f99b95c6c
= <60821161+Agua-bit@users.noreply.github.com>
2023-10-17 01:25:01 -0500

    a

--- futbolista.py
+++ futbolista.py
@@ -4,7 +4,7 @@ from deportista import Deportista
 class Futbolista(Persona, Deportista):
     listaFutbolistas = []
     def __init__(self, nombre, edad, altura, sexo, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
-        Persona.__init__(self, nombre, edad, altura, sexo)
+        super().__init__(nombre, edad, altura, sexo)
         Deportista.__init__(self, "Futbol",añosPracticando)
         self._golesMarcados = golesMarcados
         self._tarjetasRojas = tarjetasRojas

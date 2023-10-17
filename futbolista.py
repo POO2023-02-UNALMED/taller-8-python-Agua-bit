ae0efb2779ef38091c227e107a5178dd97a2c7af
= <60821161+Agua-bit@users.noreply.github.com>
2023-10-17 01:28:35 -0500

    89

--- futbolista.py
+++ futbolista.py
@@ -38,5 +38,5 @@ class Futbolista(Persona, Deportista):
         cls.listaFutbolistas = listaFutbolistas
 
     def __str__(self):
-        return "Mi nombre es " + self.get_nombre + " soy profesional en el deporte " + self.get_deporte + " Tengo " + str(self.get_edad) + " años de edad y llevo " + str(self.get_añosPracticando) + " años en el deporte" 
+        return "Mi nombre es " + self.getNombre + " soy profesional en el deporte " + self.getDeporte + " Tengo " + str(self.getEdad) + " años de edad y llevo " + str(self.getAñosPracticando) + " años en el deporte" 
     

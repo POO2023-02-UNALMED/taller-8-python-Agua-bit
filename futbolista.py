0b297c28fd502add584b9ef9ea009e9cb77b5ff7
= <60821161+Agua-bit@users.noreply.github.com>
2023-10-17 01:18:46 -0500

    as

--- futbolista.py
+++ futbolista.py
@@ -5,7 +5,7 @@ class Futbolista(Persona, Deportista):
     listaFutbolistas = []
     def __init__(self, nombre, edad, altura, sexo, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
         Persona.__init__(self, nombre, edad, altura, sexo)
-        Deportista.__init__(self, añosPracticando)
+        Deportista.__init__(self, "Futbol",añosPracticando)
         self._golesMarcados = golesMarcados
         self._tarjetasRojas = tarjetasRojas
         self._piernaHabil = piernaHabil
